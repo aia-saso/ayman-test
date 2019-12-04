a8517dfb0b5fbd9cfe4784b8057bc8e74c4a030e
Aia Mahnoud <aia@Aias-MacBook-Air.local>
2019-12-04 20:46:22 +0200

    finish task t-1

--- main.go
+++ main.go
@@ -35,10 +35,10 @@ func init() {
 var templates *template.Template
 
 func main() {
-	fmt.Println("Hello")
+	fmt.Println("Hello Aya")
 	templates = template.Must(template.ParseGlob("templates/*.html"))
 	r := mux.NewRouter()
-	http.Handle("/assets/", http.StripPrefix("/assets/", http.FileServer(http.Dir("www/assets"))))
+	http.Handle("/asset", http.StripPrefix("/assets/", http.FileServer(http.Dir("www/assets"))))
 
 	r.HandleFunc("/users", handleListUsers).Methods("GET")
 	r.HandleFunc("/user", handleViewUser).Methods("GET")
